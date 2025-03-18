4fa9e3eb56b213ad174b2f1fb3379aaca16e8b22
Leonardo Ruiz <leonardoruiz1402@gmail.com>
2025-03-17 21:54:22 -0300

    Finalização do exercicio de criação de uma função que recebe parâmetros e retorna uma mensagem personalizada

--- aula2502.ts
+++ aula2502.ts
@@ -8,4 +8,15 @@ let somaValores: number = valores.reduce((soma, valor) => soma + valor, 0);
 console.log(somaValores); // 79
 
 
+// Exercício 2
+// Crie uma função que recebe um nome e retorna uma mensagem personalizada.
+
+function mensagemPersonalizada(
+    nomeAluno: string,
+    nomeProfessor: string
+  ): string {
+    return `Olá, ${nomeAluno}! Seja bem-vindo(a)! ao Typescript com ${nomeProfessor}!`;
+  }
+  
+  console.log(mensagemPersonalizada("Leonardo Ruiz", "Hete Caetano")); // Olá, Leonardo Ruiz! Seja bem-vindo(a)! ao Typescript com Hete Caetano!
 
